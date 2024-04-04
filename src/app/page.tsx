90341dbc306d829b4a6e1bc71eed8c6b45fc1761
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-04 18:00:11 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Home</h1>
+      <h2>Teste</h2>
     </main>
   );
 }
